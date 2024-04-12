9a916d63cb8f1cf508758a17919114f941083713
sqx-afk <69343964+sqx-afk@users.noreply.github.com>
2024-04-12 17:55:04 +0800

    Update DeepMed.R

--- R/DeepMed.R
+++ R/DeepMed.R
@@ -1,5 +1,22 @@
 DeepMed=function(y,d,m,x,method="DNN",hyper_grid=NA,epochs=500,batch_size=100,trim=0.05){
 
+  if(!is.vector(y)){
+    warning("The outcome variable should be a numeric vector")
+    break
+  }
+  if(!is.vector(d)){
+    warning("The treatment variable should be a numeric vector")
+    break
+  }
+  if(!is.vector(m)|!is.matrix(m)){
+    warning("The mediator variable should be either a numeric vector or matrix")
+    break
+  }
+  if(!is.vector(x)|!is.matrix(x)){
+    warning("The exposure variable(s) should be either a numeric vector or matrix")
+    break
+  }
+
   if(method!="Lasso"){
     hyper_grid = as.data.frame(hyper_grid)
     hyper=DeepMed_cv(y,d,m,x,method,hyper_grid,epochs,batch_size)
